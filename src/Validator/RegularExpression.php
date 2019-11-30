ba15ced6b737298b52268abdbc08135861fce480
jroedel <jeff.roedel.isp@gmail.com>
2019-11-30 01:54:10 -0500

    A way to validate regular expressions

---
+++ src/Validator/RegularExpression.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace SionModel\Validator;
+
+use Zend\Validator\Regex;
+use Zend\Validator\AbstractValidator;
+
+class RegularExpression extends AbstractValidator
+{
+    public function isValid($regex)
+    {
+        try {
+            $validator = new Regex($regex);
+        } catch (\Exception $e) {
+            return false;
+        }
+        return true;
+    }
+}
