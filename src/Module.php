959ceb6ac31713d04d966c1a7a13ca36818c6f37
Jeff Roedel <jeff.roedel@schoenstatt-fathers.org>
2022-08-07 11:25:26 -0500

    Add information to Exception emails

--- src/Module.php
+++ src/Module.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace SionModel;
 
+use BjyAuthorize\Provider\Identity\ProviderInterface;
 use BjyAuthorize\Service\Authorize;
 use Exception;
+use Laminas\Mvc\Application;
 use Laminas\Mvc\ModuleRouteListener;
 use Laminas\Mvc\MvcEvent;
 use Laminas\View\Helper\Navigation\AbstractHelper;
@@ -42,9 +44,20 @@ class Module
         $eventManager->attach('dispatch.error', function ($event) {
             $exception = $event->getResult()->exception;
             if ($exception instanceof Exception) {
-                $sm      = $event->getApplication()->getServiceManager();
+                /** @var Application $app */
+                $app     = $event->getApplication();
+                $sm      = $app->getServiceManager();
+                $request = $app->getRequest();
+                /** @var ErrorHandling $service */
                 $service = $sm->get(ErrorHandling::class);
-                $service->logException($exception);
+                /** @var Authorize $authorize */
+                $authorize = $sm->get(Authorize::class);
+                $acl       = $authorize->getAcl();
+                $identity  = $authorize->getIdentity();
+                /** @var ProviderInterface $identityProvider */
+                $identityProvider = $sm->get(ProviderInterface::class);
+                $roles            = $identityProvider->getIdentityRoles();
+                $service->logException($exception, $request, $roles, $identity);
             }
         });
     }
--- src/Service/ErrorHandling.php
+++ src/Service/ErrorHandling.php
@@ -5,9 +5,13 @@ declare(strict_types=1);
 namespace SionModel\Service;
 
 use Exception;
+use Laminas\Http\PhpEnvironment\Request;
 use Laminas\Log\LoggerInterface;
+use Laminas\Stdlib\RequestInterface;
+use Laminas\Uri\Http;
 
 use function implode;
+use function is_string;
 
 class ErrorHandling
 {
@@ -15,7 +19,7 @@ class ErrorHandling
     {
     }
 
-    public function logException(Exception $e): void
+    public function logException(Exception $e, RequestInterface $request, array $roles, ?string $identity): void
     {
         $trace    = $e->getTraceAsString();
         $i        = 1;
@@ -23,8 +27,26 @@ class ErrorHandling
         do {
             $messages[] = $i++ . ": " . $e->getMessage();
         } while ($e = $e->getPrevious());
+        $log = '';
+        if ($request instanceof Request) {
+            $uri = $request->getUriString();
+            if ($uri instanceof Http) {
+                $uri = $uri->toString();
+            }
+            if (is_string($uri)) {
+                $log .= $uri . "\n";
+            }
+        }
 
-        $log  = "Exception:\n" . implode("\n", $messages);
+        if (! empty($identity)) {
+            $log .= "Identity: $identity\n";
+        }
+        try {
+            $log .= "Roles: " . implode(', ', $roles) . "\n";
+        } catch (Exception) {
+        }
+
+        $log .= "Exception:\n" . implode("\n", $messages);
         $log .= "\nTrace:\n" . $trace;
 
         $this->logger->err($log);
